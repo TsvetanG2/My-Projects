068efafa43b646f7ec5363d79cd8e16d48f0045d
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-09 12:24:22 +0300

    Add files via upload

---
+++ SoftUni C#/Basics C#/NumSequels/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NumSequels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int left = 0;
+            int right = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                left += currentNumber;
+            }
+            for (int i = 0; i < n; i ++)
+            {
+                int currentnumber = int.Parse(Console.ReadLine());
+                right += currentnumber;
+            }
+            if (left == right)
+            {
+                Console.WriteLine($"Yes, sum = {left}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(left - right)}");
+            } 
+                
+        }
+    }
+}
---
+++ SoftUni C#/Basics C#/OddSeq/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OddSeq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int oddsum = 0;
+            int evensum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evensum += number;
+                    Console.WriteLine("Yes");
+                    Console.WriteLine($"Sum = {evensum}");
+                }
+                else
+                {
+                    oddsum += number;
+                    Console.WriteLine("No");
+                    Console.WriteLine($"Diff = {Math.Abs(oddsum - evensum)}");
+                }
+
+            }
+            
+        }  
+    }
+}
