11c2592f47b1d51436c22672aaca7be36ffefd5b
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-09 12:48:29 +0300

    Add files via upload

---
+++ SoftUni C#/Fundamentals C#/Associative Arrays/A Miner Task/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new Dictionary<string, int>();
+            string resources = Console.ReadLine();
+
+            while (resources != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+                if (!items.ContainsKey(resources))
+                {
+                    items.Add(resources, 0);
+                }
+                items[resources] += quantity;
+                resources = Console.ReadLine();
+            }
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
---
+++ SoftUni C#/Fundamentals C#/Associative Arrays/Courses/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nestDictionary = new Dictionary<Dictionary<string, string>,
+                Dictionary<string, string>>();
+
+            var keyDictionary = new Dictionary<string, string>();
+            keyDictionary.Add("name", "pesho");
+            var valueDictionary = new Dictionary<string, string>();
+            valueDictionary.Add("value", "pesho");
+            nestDictionary.Add(keyDictionary, valueDictionary);
+            Console.WriteLine(nestDictionary[keyDictionary] = valueDictionary);
+
+            foreach (var nestdictionary in nestDictionary )
+            {
+                Console.WriteLine(nestdictionary.Key);
+                foreach (var kvpDictionary in nestdictionary.Value)
+                {
+                    Console.WriteLine(kvpDictionary.Key);
+                    Console.WriteLine(kvpDictionary.Value);
+                }
+                Console.WriteLine(nestdictionary.Value);
+            }
+        }
+    }
+}
