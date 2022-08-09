466bb9b8ec26d2b3fb9cd42b2f7f84c8013226b7
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-09 12:20:30 +0300

    Add files via upload

---
+++ SoftUni C#/Basics C#/ForCycle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ForCycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (n = 1; n <= 100; n += 1)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
---
+++ SoftUni C#/Basics C#/ReverseCount/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReverseCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+
+            for (int n = i ; i >= 1 ; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
