ae500e862c9330855c83b612f0f4bfa68d27fc43
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-09 11:59:59 +0300

    Add files via upload

---
+++ SoftUni C#/Basics C#/WorldRecord/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WorldRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordseconds = double.Parse(Console.ReadLine());
+            double metres = double.Parse(Console.ReadLine());
+            double secondspermetre = double.Parse(Console.ReadLine());
+
+            // Уравнения
+            double result = metres * secondspermetre;
+            double addmetre = metres / 15;
+            double result2 = addmetre * 12.5;
+            double result3 = result + result2;
+
+            
+            if (recordseconds < result3)
+            {
+
+                double finalresult = result3 - recordseconds;
+                Console.WriteLine($"No, he failed! He was {finalresult:f2} seconds slower.");
+            }
+            else 
+            {
+                
+                Console.WriteLine($"Yes, he succeeded! The new world record is {result3:f2} seconds.");
+            }
+        }
+    }
+}
