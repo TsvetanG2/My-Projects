7bab7c0309796740e279df6c69f804f8f5d5325c
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-09 12:22:58 +0300

    Add files via upload

---
+++ SoftUni C#/Basics C#/Степенуване/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Степенуване
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <=n; i +=2 )
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(Math.Pow(2, i));
+                }
+            }
+        }
+    }
+}
