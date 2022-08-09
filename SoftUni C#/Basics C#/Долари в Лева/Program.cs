6f331379d99c0ee79a1f4f4f14408187e112efd3
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-09 11:43:09 +0300

    Add files via upload

---
+++ SoftUni C#/Basics C#/Долари в Лева/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Долари_в_Лева
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double bgn = usd * 1.79549;
+
+            Console.WriteLine(bgn);
+        }
+    }
+}
---
+++ SoftUni C#/Basics C#/Озеленяване на двор/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Озеленяване_на_двор
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = 7.61;
+            double result = a * b;
+            double result2 = result * 0.18;
+            double result3 = result - result2;
+
+            Console.WriteLine($"The fInal price is: {result3} lv.");
+            Console.WriteLine($"The discount is: {result2} lv.");
+        }
+    }
+}
