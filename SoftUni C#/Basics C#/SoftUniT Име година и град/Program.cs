38bfbfb6c8a0e86c0d85156a586ba1560d1a2799
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-09 11:37:21 +0300

    Add files via upload

---
+++ SoftUni C#/Basics C#/SoftUniT Име година и град/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SoftUniTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string secondName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string town = Console.ReadLine();
+            Console.WriteLine($"Hello {firstName} {secondName}, a {age} y.o from {town}");
+
+        }
+    }
+}
