3233885a417fac4cf15e724c35fe051885eb8808
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-09 11:50:52 +0300

    Add files via upload

---
+++ SoftUni C#/Basics C#/GuessThePassword/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GuessThePassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = (Console.ReadLine());
+            string a = "s3cr3t!P@ssw0rd";
+
+
+            if (password == a)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
