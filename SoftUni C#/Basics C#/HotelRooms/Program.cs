98211f9999ef308418a5cda89e5f58e7db87a395
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-09 12:15:53 +0300

    Add files via upload

---
+++ SoftUni C#/Basics C#/HotelRooms/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HotelRooms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int nights = int.Parse(Console.ReadLine());
+
+            double price = 0;
+            string room = "Studio";
+            string room2 = "Apartment";
+            bool all = room == "Studio" || room2 == "Apartment";
+            double total = price * nights;
+
+            if (month == "May" || month == "Octomber")
+            {
+
+            }
+
+        }
+
+    }
+}
---
+++ SoftUni C#/Basics C#/OperationBetweenNumbers/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace OperationBetweenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N1 = int.Parse(Console.ReadLine());
+            int N2 = int.Parse(Console.ReadLine());
+            string symbol = Console.ReadLine();
+
+            double result = 0;
+            bool IsSymbolValid = symbol == "/" || symbol == "%";
+            bool IsResultValid = result == 0;
+
+            switch (symbol)
+            {
+                case "+":
+                    result = N1 + N2;
+                    if (result % 2 == 0)
+                    {
+                        Console.WriteLine($"{N1} {symbol} {N2} = {result} - even");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{N1} {symbol} {N2} = {result} - odd");
+                    }
+                    break;
+                case "-":
+                    result = N1 - N2;
+                    if (result % 2 == 0)
+                    {
+                        Console.WriteLine($"{N1} {symbol} {N2} = {result} - even");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{N1} {symbol} {N2} = {result} - odd");
+                    }
+                    break;
+                case "*":
+                    result = N1 * N2;
+                    if (result % 2 == 0)
+                    {
+                        Console.WriteLine($"{N1} {symbol} {N2} = {result} - even");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{N1} {symbol} {N2} = {result} - odd");
+                    }
+                    break;
+                case "/":
+                    if ( N2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {N1} by zero");
+                    }
+                    else
+                    {
+                        result = N1 / (double)N2;
+                        Console.WriteLine($"{N1} {symbol} {N2} = {result:f2}");
+                    }
+                    break;
+                case "%":
+                    if (N2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {N1} by zero");
+                    }
+                    else
+                    {
+                        result = N1 % N2;
+                        Console.WriteLine($"{N1} {symbol} {N2} = {result:f2}");
+                    }
+                    break;
+                default:
+                    result = 0;
+                    break; 
+            }
+            
+         }
+    }
+}
