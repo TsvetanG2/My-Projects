720096b975cdaca1ffe79026acdfeb9e255cd639
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-09 11:46:20 +0300

    Add files via upload

---
+++ SoftUni C#/Basics C#/По-голямо,по-малко/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace По_голямо_по_малко
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
---
+++ SoftUni C#/Basics C#/Пресмятане на аквариум/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Пресмятане_на_аквариум
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine());
+
+            double measurments = a * b * c;
+            double litres = measurments * 0.001;
+            double resultperc = percentage * 1 / 100;
+            double finalresult = litres * (1 - resultperc);
+
+            Console.WriteLine(finalresult);
+        }
+    }
+}
