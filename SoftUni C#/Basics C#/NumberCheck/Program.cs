39732175f1c629837e58d23aae52d2111c3b5cf7
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-09 12:07:55 +0300

    Add files via upload

---
+++ SoftUni C#/Basics C#/NumberCheck/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NumberCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if ( number == 0)
+            {
+                Console.WriteLine("No");
+            }     
+            else
+            {
+                Console.WriteLine("Yes");
+            }
+        }
+    }
+}
---
+++ SoftUni C#/Basics C#/Квартално магазинче/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Квартално_магазинче
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            string city = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+            double price = 0;
+            
+
+            if (city == "Sofia")
+            {
+                switch (product)
+                {
+                    case "coffee":
+                        price = 0.50;
+                        break;
+                    case "water":
+                        price = 0.80;
+                        break;
+                    case "beer":
+                        price = 1.20;
+                        break;
+                    case "sweets":
+                        price = 1.45;
+                        break;
+                    case "peanuts":
+                        price = 1.60;
+                        break;
+                    default:
+                        break;
+
+                }
+                double result = quantity * price;
+                Console.WriteLine($"{result:F2}");
+            }              
+            else if (city == "Plovdiv")
+            {
+                switch (product)
+                {
+                    case "coffee":
+                        price = 0.40;
+                        break;
+                    case "water":
+                        price = 0.70;
+                        break;
+                    case "beer":
+                        price = 1.15;
+                        break;
+                    case "sweets":
+                        price = 1.30;
+                        break;
+                    case "peanuts":
+                        price = 1.50;
+                        break;
+                    default:
+                        break;
+                }
+                double result = quantity * price;
+                Console.WriteLine($"{result:F2}");
+            }
+            else if (city == "Varna")
+            {
+                switch (product)
+                {
+                    case "coffee":
+                        price = 0.45;
+                        break;
+                    case "water":
+                        price = 0.70;
+                        break;
+                    case "beer":
+                        price = 1.10;
+                        break;
+                    case "sweets":
+                        price = 1.35;
+                        break;
+                    case "peanuts":
+                        price = 1.55;
+                        break;
+                    default:
+                        break;
+                }
+                double result = quantity * price;
+                Console.WriteLine($"{result:F2}");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
