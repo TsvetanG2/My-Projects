fbe4e31f871e310bbbc030401e7a094450a93333
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-09 12:32:46 +0300

    Add files via upload

---
+++ SoftUni C#/Fundamentals C#/Lists/ListNames Exercise/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListNames_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+
+            names.Add("Petar");
+            names.Add("Yasenr");
+            names.Add("Bobby");
+            names.Add("Koko");
+            names.Add("Des");
+            names.Add("Marian");
+
+            //foreach (var name in names)
+            //{
+            //     Console.WriteLine(name);
+            // }
+
+            // Друг Метод
+
+            Console.WriteLine(string.Join(", ", names));
+
+            names.Insert(1, "Ceco");
+
+            names.Remove("Koko");
+
+            Console.WriteLine(string.Join(", ", names));
+            Console.WriteLine($"Count: {names.Count}");
+
+            bool isNameInList = names.Contains("Petar");
+
+            if (isNameInList)
+            {
+                Console.WriteLine("Petar is in the list");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+            names.Sort();
+            names.Reverse();
+            Console.WriteLine(string.Join(", ", names));
+
+        }
+    }
+}
