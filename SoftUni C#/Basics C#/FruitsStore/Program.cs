999ddefc041c0b9e3e510db83dca5c37460e1901
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-09 12:12:53 +0300

    Add files via upload

---
+++ SoftUni C#/Basics C#/FruitsStore/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace FruitsStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string item = Console.ReadLine();
+            string day = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+            double price = 0;
+
+            if (day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday")
+            {
+                switch (item)
+                {
+                    case "banana":
+                        price = 2.50;
+                        break;
+                    case "apple":
+                        price = 1.20;
+                        break;
+                    case "orange":
+                        price = 0.85;
+                        break;
+                    case "grapefruit":
+                        price = 1.25;
+                        break;
+                    case "kiwi":
+                        price = 2.70;
+                        break;
+                    case "pineapple":
+                        price = 5.50;
+                        break;
+                    case "grapes":
+                        price = 3.85;
+                        break;
+                    default:
+                        Console.WriteLine("error");
+                        break;
+                }
+                price = quantity * price;
+                Console.WriteLine($"{price:F2}");
+            }
+            else
+            {
+                if (day == "Saturday")
+                {
+                    switch (item)
+                    {
+                        case "banana":
+                            price = 2.70;
+                            break;
+                        case "apple":
+                            price = 1.25;
+                            break;
+                        case "orange":
+                            price = 0.90;
+                            break;
+                        case "grapefruit":
+                            price = 1.60;
+                            break;
+                        case "kiwi":
+                            price = 3.00;
+                            break;
+                        case "pineapple":
+                            price = 5.60;
+                            break;
+                        case "grapes":
+                            price = 4.20;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    price = quantity * price;
+                    Console.WriteLine($"{price:F2}");
+                }
+                else if (day == "Sunday")
+                {
+                    switch (item)
+                    {
+                        case "banana":
+                            price = 2.70;
+                            break;
+                        case "apple":
+                            price = 1.25;
+                            break;
+                        case "orange":
+                            price = 0.90;
+                            break;
+                        case "grapefruit":
+                            price = 1.60;
+                            break;
+                        case "kiwi":
+                            price = 3.00;
+                            break;
+                        case "pineapple":
+                            price = 5.60;
+                            break;
+                        case "grapes":
+                            price = 4.20;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    price = quantity * price;
+                    Console.WriteLine($"{price:F2}");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+                
+            }
+        }
+    }
+}
---
+++ SoftUni C#/Basics C#/TradeComision/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace TradeComision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+
+            bool IsCityValid = town == "Sofia" || town == "Plovdiv" || town == "Varna";
+            bool isSaleValid = sales >= 0;
+
+            if (!(IsCityValid) || !isSaleValid)
+            {
+                Console.WriteLine("error");
+            }
+            double result = 0;
+            if (town == "Sofia")
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    result = sales * 0.05;
+                }
+                else if (sales > 500 && sales <= 1000)
+                {
+                    result = sales * 0.07;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    result = sales * 0.08;
+                }
+                else if (sales > 10000)
+                {
+                    result = sales * 0.12;
+                }
+                Console.WriteLine($"{result:F2}");
+            }
+            else if (town == "Varna")
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    result = sales * 0.045;
+                }
+                else if (sales > 500 && sales <= 1000)
+                {
+                    result = sales * 0.075;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    result = sales * 0.10;
+                }
+                else if (sales > 10000)
+                {
+                    result = sales * 0.13;
+                }
+                Console.WriteLine($"{result:F2}");
+            }
+            else if (town == "Plovdiv")
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    result = sales * 0.055;
+                }
+                else if (sales > 500 && sales <= 1000)
+                {
+                    result = sales * 0.08;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    result = sales * 0.12;
+                }
+                else if (sales > 10000)
+                {
+                    result = sales * 0.145;
+                }
+                Console.WriteLine($"{result:F2}");
+            }
+            Console.WriteLine();        
+        }
+    }
+}
