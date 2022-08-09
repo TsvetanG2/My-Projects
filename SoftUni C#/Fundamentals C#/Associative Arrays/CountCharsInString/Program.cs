cda34bb06bdf081d951afa4083a1ef6a1b706183
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-09 12:52:20 +0300

    Add files via upload

---
+++ SoftUni C#/Fundamentals C#/Associative Arrays/CountCharsInString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountCharsInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] word = Console.ReadLine().ToCharArray(); // "text" ще изглежда = ['t','e','x','t']
+
+            Dictionary<char, int> letters = new Dictionary<char, int>(); // charL int: => [char 't': int 2]
+
+            foreach (var letter in word)
+            {
+                if (letter != ' ')
+                {
+                    if (!letters.ContainsKey(letter)) // letters.ContainsKey('t') - имаш ли 't'
+                    {
+                        letters[letter] = 0;
+                    }
+                    letters[letter]++; // ако имаш тази буквичка искам да добавиш 
+                }
+            }
+            foreach (var kvpLetter in letters)
+            {
+                Console.WriteLine($"{kvpLetter.Key} -> {kvpLetter.Value}");
+            }
+        }
+    }
+}
