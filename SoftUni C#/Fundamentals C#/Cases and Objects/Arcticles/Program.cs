0cd8a3f93a0bfe7697289755fe908f6a4c98d31b
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-09 12:35:41 +0300

    Add files via upload

---
+++ SoftUni C#/Fundamentals C#/Cases and Objects/Arcticles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Arcticles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newArcitcle = new Arcticle(title: "some title", content: "some content",author: "some author");
+
+            Console.WriteLine(newArcitcle);
+            
+        }
+    }
+
+    class Arcticle
+    {
+
+        public Arcticle(string title, string content, string author)
+        {
+            Author = author;
+            Title = title;
+            Content = content;
+        }
+        public  string Title { get; set;}
+
+        public string Content { get; set;}
+
+        public string Author { get; set;}
+
+        public void ChangeAuthor(string author) => Author = author;
+
+        public void Edit(string content) => Content = content;
+
+        public void Rename(string title) => Title = title;
+
+
+        public override string ToString() => $"{Title} - {Content}. {Author}";
+    }
+    
+    
+}
