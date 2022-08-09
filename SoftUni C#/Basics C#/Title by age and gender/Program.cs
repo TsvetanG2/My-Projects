91cdaf7d0fbdef5776374a64ab931d3e008e86d9
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-09 12:05:30 +0300

    Add files via upload

---
+++ SoftUni C#/Basics C#/Title by age and gender/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Title_by_age_and_gender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var age = int.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            if (gender == "m")
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else
+                {
+                    Console.WriteLine("Master");
+                }
+            }
+            else
+            {
+                if ( age >= 16)
+                {
+                    Console.WriteLine("Ms.");
+                }
+                else
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+            
+        }           
+
+    }
+}
