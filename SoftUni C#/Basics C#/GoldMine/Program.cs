82d8bb9e1c8ebbd787317de219921feaf792791b
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-08-20 23:28:45 +0300

    Add files via upload

---
+++ SoftUni C#/Basics C#/GoldMine/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GoldMine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int locations = int.Parse(Console.ReadLine());
+
+            double goldfortheday = 0;
+
+            for (int i = 1; i <= locations; i++)
+            {
+                double expectedgold = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+
+                while (goldfortheday != 0)
+                {
+                    double diggedgold = double.Parse(Console.ReadLine());
+
+                    if (diggedgold >= 0)
+                    {
+                        diggedgold += goldfortheday;
+                    }
+
+                }
+
+                goldfortheday /= days;
+
+                if (expectedgold <= goldfortheday)
+                {
+                    Console.WriteLine($"Good job! Average gold per day: {goldfortheday:f2}.");
+                }
+                else
+                {
+                    goldfortheday -= expectedgold;
+
+                    Console.WriteLine($"You need: {Math.Abs(goldfortheday):f2}.");
+                }
+                    
+                        
+            }
+        }
+    }
+}
