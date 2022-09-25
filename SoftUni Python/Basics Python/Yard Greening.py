b4bd76e98559de2003f334baa99c4f40f186ef6d
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2022-09-25 19:22:37 +0300

    Delete Yard Greening.py

--- SoftUni Python/Basics Python/Yard Greening.py
+++
@@ -1,8 +0,0 @@
-squares = float(input())
-SquarePrice = 7.61
-Result = squares * SquarePrice
-Discounted = Result * 0.18
-DiscountedPrice = Result - Discounted
-
-print(f'The final price is: {DiscountedPrice} lv.')
-print(f'The discount is: {Discounted} lv.')
