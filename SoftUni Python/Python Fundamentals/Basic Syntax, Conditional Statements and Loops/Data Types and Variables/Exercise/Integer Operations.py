23af0bf94aa5bd3ce7f918f2d9eeadbf3b87dd04
Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>
2023-01-27 12:54:24 +0200

    Add files via upload
    Signed-off-by: Tsvetan Gerginov <106432651+TsvetanG2@users.noreply.github.com>

---
+++ SoftUni Python/Python Fundamentals/Basic Syntax, Conditional Statements and Loops/Data Types and Variables/Exercise/Integer Operations.py
@@ -0,0 +1,10 @@
+number_1 = int(input())
+number_2 = int(input())
+number3 = int(input())
+number4 = int(input())
+
+result1 = number_1 + number_2
+result2 = result1 // number3
+result3 = int(result2 * number4)
+print(result3)
+
---
+++ SoftUni Python/Python Fundamentals/Basic Syntax, Conditional Statements and Loops/Data Types and Variables/Exercise/Party Profit.py
@@ -0,0 +1,24 @@
+companion = int(input())
+days = int(input())
+
+coins = 0
+
+for day in range(1, days + 1):
+
+    if day % 10 == 0:
+        companion -= 2
+
+    if day % 15 == 0:
+        companion += 5
+        coins -= 2 * companion
+
+    coins += 50 - 2 * companion
+
+    if day % 3 == 0:
+        coins -= 3 * companion
+
+    if day % 5 == 0:
+        coins += 20 * companion
+
+coins = coins // companion
+print(f'{companion} companions received {coins} coins each.')
